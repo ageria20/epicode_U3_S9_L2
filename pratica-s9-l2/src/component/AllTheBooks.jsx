da3238f7fc27f81df595f09355a4db19ed11d267
Andrea <ageria20@gmail.com>
2024-06-25 16:58:54 +0200

    cart feature

--- pratica-s9-l2/src/component/AllTheBooks.jsx
+++ pratica-s9-l2/src/component/AllTheBooks.jsx
@@ -13,13 +13,20 @@ import WelcomeAlert from "./Welcome";
 class AllTheBooks extends Component {
 
     state = {
-        library: null
+        library: null,
+        cartCount: 0
     }
 
     render() {
         return (
 
             <>
+            <Button  variant="primary">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+</svg><Badge bg="secondary">{this.state.cartCount}</Badge>
+                <span className="visually-hidden"></span>
+             </Button>
             <div className="d-flex justify-content-between container mx-auto mb-5">
             <Button variant="primary" onClick={() => this.setState({library: fantasy })
             }>Fantasy</Button>
@@ -44,7 +51,7 @@ class AllTheBooks extends Component {
               <Card.Text>
                     <div>{book.category}</div>
               </Card.Text>
-              <Button  variant="primary">
+              <Button variant="primary" onClick={() => this.setState({ cartCount: this.state.cartCount + 1})}>
                 Buy <Badge bg="secondary">${book.price}</Badge>
                 <span className="visually-hidden"></span>
              </Button>
